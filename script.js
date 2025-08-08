c74a28736ce84c125a8c73a395b83060e06addcc
Saptarshi Bisoi <143985222+saptarshi-bisoi@users.noreply.github.com>
2025-08-08 19:16:11 +0530

    random size, position, animation delay

--- script.js
+++ script.js
@@ -47,3 +47,15 @@ function createParticles() {
   for (let i = 0; i < particleCount; i++) {
     const particle = document.createElement("div");
     particle.className = "particle";
+    
+const size = Math.random() * 4 + 2;
+    particle.style.width = size + "px";
+    particle.style.height = size + "px";
+    particle.style.left = Math.random() * 100 + "%";
+    particle.style.top = Math.random() * 100 + "%";
+
+    particle.style.animationDelay = Math.random() * 8 + "s";
+
+    particlesContainer.appendChild(particle);
+  }
+}
