43e19120bca909efa8b385aeb919a5bd8195cf78
Saptarshi Bisoi <143985222+saptarshi-bisoi@users.noreply.github.com>
2025-08-08 19:08:34 +0530

    Add smooth scroll for nav links

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function toggleTheme() {
     themeIcon.className = "fas fa-sun";
   }
 }
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  });
+});
