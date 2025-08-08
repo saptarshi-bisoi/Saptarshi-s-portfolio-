ba3d22a750ec3eba2f6e16e376761aa30916e4d0
Saptarshi Bisoi <143985222+saptarshi-bisoi@users.noreply.github.com>
2025-08-08 19:41:52 +0530

    Contact form submission

--- script.js
+++ script.js
@@ -109,3 +109,12 @@ window.addEventListener("scroll", function () {
     }
   });
 });
+  
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+ 
